370c26655c7552a4f9fe53c5cd6f766340f546eb
吴静 <wujing@wujingdeMacBook-Pro.local>
2026-05-09 08:08:23 +0800

    Use separate application id for J Reader

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,7 +47,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "io.legato.kazusa"
+        applicationId = "io.legato.kazusa.jread"
         minSdk = 26
         targetSdk = 34
         versionCode = System.getenv("COMMIT_NUMBER")?.toInt()?.let { 10000 + it } ?: 32640
